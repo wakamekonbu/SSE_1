64ef3d6f2954ff4723c57882af76023d37886084
垣谷悠斗 <hr.8714hmg@icloud.com>
2018-08-02 14:20:20 +0900

    revise

--- object.py
+++ object.py
@@ -4,6 +4,7 @@ import numpy as np
 from numpy.random import *
 import hashlib
 
+
 # テキストファイルかどうかをチェックする
 # 対象となるファイルが .txt ならTrue、それ以外なら Falseを返す。
 def text_checker(name):
@@ -13,26 +14,29 @@ def text_checker(name):
     else:
         return False
 
+
 # テキストファイルの文字列を単語に分割してリスト化する
-def word_list(list,text):
-    f = open(os.path.join("files",text))
+def word_list(list, text):
+    f = open(os.path.join("files", text))
     data = f.read()
     # 空白文字列で区切る。
     for word in data.split():
-        word = word.lower() # 大文字 -> 小文字へ
-        word = word.replace(".","") # ピリオドを消去
-        word = word.replace(",","") # カンマを消去
-        list.append(word) # listにwordを追加する
+        word = word.lower()  # 大文字 -> 小文字へ
+        word = word.replace(".", "")  # ピリオドを消去
+        word = word.replace(",", "")  # カンマを消去
+        list.append(word)  # listにwordを追加する
 
-indlen=100
+indlen = 100
+
+
 class Client:
-    _s_=57
-    _filenum_=0
+    _s_ = 57
+    _filenum_ = 0
     _prg_ = None
 
     def __init__(self):
         seed(self._s_)
-        self._prg_=randint(0,2,100)
+        self._prg_ = randint(0, 2, 100)
 
     def _tp_(self, keyword):
         # ハッシュ化されたkeywordから添字を得る
@@ -46,18 +50,18 @@ class Client:
         # sha256というハッシュ関数を用いて、単語の16進数のハッシュ値を生成
         hash_word_s = hashlib.sha256(lr.encode('utf-8')).hexdigest()
         # hash_word_sをint型へ変換し、10000で割った余りを計算
-        hash_word_st = int(hash_word_s, 16) % 10000 # ←tpでmod100するのでいらないのでは。
-            
+        hash_word_st = int(hash_word_s, 16) % 10000  # ←tpでmod100するのでいらないのでは。
+
         # q: インデックスの添字
         q = self._tp_(hash_word_st)
 
         seed(self._s_)
         # prg: マスク共通鍵
-        prg = randint(0, 2, indlen) # 0 or 1 の100個の整数の乱数を得る
+        prg = randint(0, 2, indlen)  # 0 or 1 の100個の整数の乱数を得る
         prg_st = prg[q]
 
-        return q, prg_st # return (q, prg_st)　
-    
+        return q, prg_st  # return (q, prg_st)　
+
     def _searchInd_(self, keyword, fileidx):
         # s 乱数のシード
         # keyword 検索文字列
@@ -68,29 +72,29 @@ class Client:
         prg_st = int(searchToken[1])
         li = int(server.search(q, fileidx))
 
-        #マスク鍵と検索結果の排他的論理和
+        # マスク鍵と検索結果の排他的論理和
         ans = int((li + prg_st) % 2)
-        return ans # keyword が存在すれば 1, しなければ 0 を返す
-    
+        return ans  # keyword が存在すれば 1, しなければ 0 を返す
+
     def searchAll(self, keyword):
         for i in range(self._filenum_):
-            print("file {}: {}".format(i, self._searchInd_(keyword, i)))
-    
+            print("file {}: {}".format(i, bool(self._searchInd_(keyword, i))))
+
     def _getMask_(self, q, fileidx):
         # fileidxファイルのq番目の要素に対応するマスク(0 or 1)を返す
         return self._prg_[q]
         # これがfileidxによって異なる値にならないと，類似度が出せてしまう
 
     def _genIndex_(self, text):
-        words=[]
+        words = []
         word_list(words, text)
-        di=[] # text に含まれる単語のハッシュのリスト
-        ind=np.zeros(indlen)
+        di = []  # text に含まれる単語のハッシュのリスト
+        ind = np.zeros(indlen)
         for w in words:
             # sha256というハッシュ関数を用いて、単語の16進数のハッシュ値を生成
-            hash_word = hashlib.sha256(w.encode('utf-8')).hexdigest() # hash_wordはstr型
+            hash_word = hashlib.sha256(w.encode('utf-8')).hexdigest()  # hash_wordはstr型
             # ハッシュ値を整数に変換
-            hash_word_10 = int(hash_word,16)
+            hash_word_10 = int(hash_word, 16)
             # 衝突したら弾く（つまりその単語は検索不可能）
             if hash_word_10 % 10000 not in di:
                 di.append(hash_word_10 % 10000)
@@ -110,32 +114,31 @@ class Client:
     
     def uploadFile(self, text):
         server.uploadIndex(self._genIndex_(text))
-        self._filenum_+=1
+        self._filenum_ += 1
         print("client: successfully uploaded '"+text+"'")
 
 
-
-
 class Server:
-    _Inds_=[] #np.zeros(indlen) for i in range(filenum)
+    _Inds_ = []  # np.zeros(indlen) for i in range(filenum)
 
     def search(self, q, fileidx):
         # q: trapdoor
         # fileidx: 検索するファイルの添字（サンプルなら0か1）
         return self._Inds_[fileidx][q]
-    
+
     def uploadIndex(self, ind):
         self._Inds_.append(ind)
-        
+
 
 client = Client()
 server = Server()
 
 # カレントディレクトリ(os.getcwd())にあるファイルを取得する(os.listdir)
-for file in os.listdir(os.path.join(os.getcwd(),"files")):
+for file in os.listdir(os.path.join(os.getcwd(), "files")):
     if text_checker(file):
         client.uploadFile(file)
 
+print("If you want to exit, please type 'exit()' or 'quit()'")
 while True:
     keyword = input("search: ")
     if(keyword == "exit()" or keyword == "quit()"):
--- sample2.py
+++ sample2.py
@@ -1,9 +1,12 @@
+# 共通鍵型検索可能暗号
+
 import os
 import re
 import numpy as np
 from numpy.random import *
 import hashlib
 import tkinter as tk
+
 
 # テキストファイルかどうかをチェックする
 # 対象となるファイルが .txt ならTrue、それ以外なら Falseを返す。
@@ -14,48 +17,54 @@ def text_checker(name):
     else:
         return False
 
+
 # テキストファイルの文字列を単語に分割してリスト化する
-def word_list(list,text):
+def word_list(list, text):
     f = open(text)
     data = f.read()
     # 空白文字列で区切る。
     for word in data.split():
-        word = word.lower() # 大文字 -> 小文字へ
-        word = word.replace(".","") # ピリオドを消去
-        word = word.replace(",","") # カンマを消去
-        list.append(word) # listにwordを追加する
+        word = word.lower()  # 大文字 -> 小文字へ
+        word = word.replace(".", "")  # ピリオドを消去
+        word = word.replace(",", "")  # カンマを消去
+        list.append(word)  # listにwordを追加する
 
+
+# 0~99の値でtrapdoorが生成される
+# skは秘密鍵
 def tp(sk, keyword):
     q = (sk*sk + keyword) % 100
     return q
 
-def st(s,keyword):
+
+def st(s, keyword):
     # 小文字へ
     lr = keyword.lower()
     # sha256というハッシュ関数を用いて、単語の16進数のハッシュ値を生成
     hash_word_s = hashlib.sha256(lr.encode('utf-8')).hexdigest()
     # hash_word_sをint型へ変換し、10000で割った余りを計算
     hash_word_st = int(hash_word_s, 16) % 10000
-    
+
     q = tp(s, hash_word_st)
     seed(s)
-    prg = randint(0, 2, 100) # 0 or 1 の100個の整数の乱数を得る
+    prg = randint(0, 2, 100)  # 0 or 1 の100個の整数の乱数を得る
     prg_st = prg[q]
-    return q,prg_st # return (q, prg_st)　
+    return q, prg_st  # q: trapdoor, prg_st: mask number
+
 
 # 単語がファイルに存在するか探索する
-def search(s,keyword,list):
-    searchToken = st(s,keyword)
+def search(s, keyword, list):
+    searchToken = st(s, keyword)
     q = int(searchToken[0])
-    prg_st = int(searchToken[1])
+    prg_st = int(searchToken[1])  # 0or1の乱数
     li = int(list[q])
-    ans = int((li + prg_st) % 2)
-    return ans # keyword が存在すれば 1, しなければ 0 を返す
+    ans = int((li + prg_st) % 2)  # liに対する2回目のXOR
+    return ans  # keyword が存在すれば 1, しなければ 0 を返す
 
 # ここから-----------------------------------------------------------------------------
 # txtファイルに存在する単語を格納する空のリスト（単語リスト）を作成
 Dict = []
-DI = [[] for i in range(2)] #[[], []]と同じ。 DI は DirectIndex
+DI = [[] for i in range(2)]  # [[], []]と同じ。 DIはDirectIndex. ファイルは2個しかないことを想定
 counter = -1
 
 # カレントディレクトリ(os.getcwd())にあるファイルを取得する(os.listdir)
@@ -64,51 +73,49 @@ for file in os.listdir(os.getcwd()):
     if text_checker(file):
         counter = counter + 1
         words = []
-        word_list(words, file) # file に存在する単語を words に格納する
+        word_list(words, file)  # file に存在する単語を words に格納する
         for w in words:
             # sha256というハッシュ関数を用いて、単語の16進数のハッシュ値を生成
-            hash_word = hashlib.sha256(w.encode('utf-8')).hexdigest() # hash_wordはstr型
+            hash_word = hashlib.sha256(w.encode('utf-8')).hexdigest()  # hash_wordはstr型
             # ハッシュ値を整数に変換
-            hash_word_10 = int(hash_word,16)
-            # 衝突したら弾く
+            hash_word_10 = int(hash_word, 16)
+            # 衝突したら弾く(DI内になければ追加する)
             if hash_word_10 % 10000 not in DI[counter]:
                 DI[counter].append(hash_word_10 % 10000)
-            
+
             # Dict に単語を重複しないように追加する
             if w not in Dict:
                 Dict.append(w)
     else:
         pass
 
-Ind1 = np.zeros(100) # Ind1 = [0, 0, 0, .... 0]
-Ind2 = np.zeros(100) # Ind2 = [0, 0, 0, .... 0]
-s = 57 # 乱数のシード
-seed(s) # 擬似乱数生成の元となる値(s)を適用。このsの値を変えることで生成される乱数の値も変わる
-prg = randint(0,2,100) # 0 or 1 の100個の整数の乱数を得る
+Ind1 = np.zeros(100)  # Ind1 = [0, 0, 0, .... 0], 1つ目のファイルに含まれるwordのハッシュを記録, サーバーが保持
+Ind2 = np.zeros(100)  # Ind2 = [0, 0, 0, .... 0], 2つ目のファイルに含まれるwordのハッシュを記録, サーバーが保持
+s = 57  # 乱数のシード
+seed(s)  # 擬似乱数生成の元となる値(s)を適用。このsの値を変えることで生成される乱数の値も変わる
+prg = randint(0, 2, 100)  # 0 or 1 の100個の整数の乱数を得る, ハッシュの暗号化用乱数
 
 for i in range(len(DI[0])):
     q = tp(s, DI[0][i])
     Ind1[q] = 1
-    i = i + 1
 
 for i in range(len(DI[1])):
     q = tp(s, DI[1][i])
     Ind2[q] = 1
-    i= i + 1
 
 for i in range(len(Ind1)):
-    pr = int(prg[i])
-    I1 = int(Ind1[i])
+    pr = int(prg[i])  # 0or1の整数
+    I1 = int(Ind1[i])  # 単語があれば1, 無ければ0
     I2 = int(Ind2[i])
-    Ind1[i] = int((I1+pr) % 2)
-    Ind2[i] = int((I2+pr) % 2)
+    Ind1[i] = int((I1+pr) % 2)  # ここからサーバーが保持
+    Ind2[i] = int((I2+pr) % 2)  # ここからサーバーが保持
 
 # 出力
 print("Dict")
 print(Dict)
-searchword= 'cryptography'
-result1 = search(s,searchword,Ind1)
-result2 = search(s,searchword,Ind2)
+searchword = input('Please type a word you want to search: ')
+result1 = search(s, searchword, Ind1)
+result2 = search(s, searchword, Ind2)
 print("result1:", result1)
 print("result2:", result2)
 
@@ -118,7 +125,7 @@ root.title(u"Sever has")
 root.geometry("400x300")
 
 label = tk.Label(root, text="Index1")
-#表示
+# 表示
 label.grid()
 label_Ind1 = tk.Label(root, text=Ind1)
 label_Ind1.grid()
@@ -126,23 +133,32 @@ label2 = tk.Label(root, text="Index2")
 label2.grid()
 label_Ind2 = tk.Label(root, text=Ind2)
 label_Ind2.grid()
-Ind2_print=Ind1
-#label_Ind3 = tk.Label(root, text='[{0}{1}{2}]'.format(Ind2_print[0:int(st(s,searchword)[0])-1],Ind2_print[int(st(s,searchword)[0])],Ind2[int(st(s,searchword)[0]+1):len(Ind2)]))
-#label_Ind3.grid()
+Ind2_print　= Ind1
+# label_Ind3 = tk.Label(root, text='[{0}{1}{2}]'.format(Ind2_print[0:int(st(s,searchword)[0])-1],
+#              Ind2_print[int(st(s, searchword)[0])],Ind2[int(st(s,searchword)[0]+1):len(Ind2)]))
+# label_Ind3.grid()
 
 root_1 = tk.Tk()
 root_1.title(u"Sever searches")
 root_1.geometry("400x300")
-#label_1 = tk.Label(root_1, text="Server receives")
+# label_1 = tk.Label(root_1, text="Server receives")
 label_2 = tk.Label(root_1, text='server receives search token:')
-label_3 = tk.Label(root_1, text='st={0}, prn={1}'.format(st(s,searchword)[0],st(s,searchword)[1]))
-label_empty=tk.Label(root_1, text='')
-label_search=tk.Label(root_1, text='search execution')
-label_4 = tk.Label(root_1, text='Index1[{0}] XOR prn={1} XOR {2}={3}'.format(int(st(s,searchword)[0]),int(Ind1[st(s,searchword)[0]]),st(s,searchword)[1],search(s,searchword,Ind1)))
-label_5 = tk.Label(root_1, text='Index2[{0}] XOR prn={1} XOR {2}={3}'.format(int(st(s,searchword)[0]),int(Ind2[st(s,searchword)[0]]),st(s,searchword)[1],search(s,searchword,Ind2)))
+label_3 = tk.Label(root_1, text='st={0}, prn={1}'.format(st(s, searchword)[0], st(s, searchword)[1]))
+label_empty　= tk.Label(root_1, text='')
+label_search　= tk.Label(root_1, text='search execution')
+label_4 = tk.Label(root_1,
+                   text='Index1[{0}] XOR prn={1} XOR {2}={3}'.format(int(st(s, searchword)[0]),
+                                                                     int(Ind1[st(s, searchword)[0]]),
+                                                                     st(s, searchword)[1],
+                                                                     search(s, searchword, Ind1)))
+label_5 = tk.Label(root_1,
+                   text='Index2[{0}] XOR prn={1} XOR {2}={3}'.format(int(st(s, searchword)[0]),
+                                                                     int(Ind2[st(s, searchword)[0]]),
+                                                                     st(s, searchword)[1],
+                                                                     search(s, searchword, Ind2)))
 
-#表示
-#label_1.grid()
+# 表示
+# label_1.grid()
 label_2.grid()
 label_3.grid()
 label_empty.grid()
